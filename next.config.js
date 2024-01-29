ad9be141f727f131523fc0d1271e2851f7a52bd8
Ekaterina_Bulgakova <carefulka@gmail.com>
2024-01-29 13:15:37 +0300

    Fix next.config: pageExtensions and output

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "md", "ts", "tsx"],
+  output: "export",
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
